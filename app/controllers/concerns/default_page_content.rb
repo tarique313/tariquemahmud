a83c28b337348dfda5035411dc2b3896c2db6609
tarique313 <mahmud.tarique@gmail.com>
2017-06-18 15:51:09 +0600

    Added navbar and header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+	extend ActriveSupport::Concern
+
+	included do
+		before_filter :set_page_defaults
+	end 
+
+	def set_page_defaults
+		@page_title = "Gazi Tarique Mahmud | My Portfolio Website"
+		@seo_keywords = "Gazi Tarique Mahmmud, Ruby, Ruby on Rails 5, Ruby on Rails, Bangladesh, Ruby on Rails Bangladesh, Paribagh, Dhaka, RoR"
+	end
+end
