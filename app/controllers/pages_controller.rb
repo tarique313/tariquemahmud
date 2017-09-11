bcda9d2e524d654cb2274c6d5dbd8a734a9271c7
tarique313 <mahmud.tarique@gmail.com>
2017-09-11 18:37:27 +0600

    Small changs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
+  
+
   def home
-   
     @skills = Skill.all
   end
 
   def about
+  
   end
 
   def contact
+  
   end
+
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,56 +1,60 @@
 class PortfoliosController < ApplicationController
+
 before_action :set_portfolio_items, only: [:edit, :update, :show, :destroy]
 layout "portfolio"
-  access all: [:show, :index, :ror, :pentest_portfolio_items], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
-  def index
-    @portfolio_items = Portfolio.all
-  end
+access all: [:show, :index, :ror, :pentest_portfolio_items], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
-  def ror
-    @ror_portfolio_items = Portfolio.ror
-  end
+def index
+  @portfolio_items = Portfolio.all
+end
 
-  def pentest_portfolio_items
-    @pentest_portfolio_items = Portfolio.pentest_portfolio_items
-  end
+def ror
+  @ror_portfolio_items = Portfolio.ror
+end
 
-  def new 
-    @portfolio_item = Portfolio.new
-    4.times { @portfolio_item.technologies.build}
-  end
+def pentest_portfolio_items
+  @pentest_portfolio_items = Portfolio.pentest_portfolio_items
+end
 
-  def create
-    @portfolio_item = Portfolio.new(portfolio_params)
-    respond_to do |format|
+def new 
+  @portfolio_item = Portfolio.new
+  4.times { @portfolio_item.technologies.build}
+end
+
+def create
+  @portfolio_item = Portfolio.new(portfolio_params)
+  respond_to do |format|
     if @portfolio_item.save!
       format.html { redirect_to @portfolio_item, notice: "Your portfolio is now live!"}
     else  
       format.html { render :new }
     end
-    end
   end
+end
 
-  def edit
-    #4.times { @portfolio_item.technologies.build}
-  end
+def edit
+  #4.times { @portfolio_item.technologies.build}
+end
 
-  def show
-  end
-  def destroy
+def show
 
-    @portfolio_item.destroy
+end
+
+def destroy
+  @portfolio_item.destroy
     respond_to do |format|
       format.html{redirect_to portfolios_url, notice: "Record was removed."}
     end
+end
+
+def update
+  if @portfolio_item.update(portfolio_params)
+    redirect_to portfolios_path, notice: "Portfolio updated successfully!"
+  else
+    render :edit
   end
-  def update
-    if @portfolio_item.update(portfolio_params)
-      redirect_to portfolios_path, notice: "Portfolio updated successfully!"
-    else
-      render :edit
-    end
-  end
+end
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
