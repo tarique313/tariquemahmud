febf38fb04aa8ac3a29a308d7426eb7f46fb05c5
Gazi Tarique Mahmud <mahmud.tarique@gmail.com>
2018-02-13 16:32:51 +0600

    Created comment broadcast.

---
+++ app/jobs/comment_broadcast_job.rb
@@ -0,0 +1,11 @@
+class ApplicationBroadcastJob < ApplicationJob
+	queue_as :default 
+
+	def perform(comment)
+		ActionCable.server.broadcast "blog_#{comment.blog.id}_channel", comment: render_comment(comment)
+	end
+	private
+
+	def render_comment(comment)
+		CommentsController.render partial: 'comments/comment', locals: {comment: comment }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
+  
+  mount ActionCable.server => '/cable'
+  
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
