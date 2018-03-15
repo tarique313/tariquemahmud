822ab6aa58820bfb9e71b320509ddca07fcd1afb
Gazi Tarique Mahmud <mahmud.tarique@gmail.com>
2018-03-16 00:06:54 +0600

    Implemented Topic index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  resources :topics, only: [:index, :show]
   resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :blogs do
@@ -19,5 +21,4 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
